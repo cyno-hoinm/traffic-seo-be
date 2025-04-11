83df3313a6910792736a122481d3d3d4843a8969
cyno-hoinm <hoinm.cyno@gmail.com>
2025-04-11 19:48:39 +0700

    last

--- src/app.ts
+++ src/app.ts
@@ -14,56 +14,38 @@ import { configureMiddleware } from "./middleware";
 dotenv.config();
 
 // PORT and Environment
-const PORT =  process.env.PORT || 3000;
-const isDev = process.env.NODE_ENV === "development";
+const PORT = process.env.PORT || 3000;
+const isDev = true
+// process.env.NODE_ENV === "development";
 const numCPUs = os.cpus().length;
 
 const debugApp = debug("app");
 
-if (cluster.isPrimary && !isDev) {
-  // logger.info(`Primary process ${process.pid} is running`);
-  // debugApp(`Starting primary process with ${numCPUs} CPU cores`);
+// Start the HTTP server in this worker
+const app = express();
+// Configure middleware
+configureMiddleware(app);
+// // Configure routes
+configureRoutes(app);
+const server = app.listen(PORT, async () => {
+  try {
+    debugApp(`Worker ${process.pid} attempting to connect to DB and Redis`);
+    await connectDB();
+    logger.info(`Worker ${process.pid} started on port ${PORT}`);
 
-  // const appWorkers = Math.ceil(numCPUs / 2);
-  // for (let i = 0; i < appWorkers; i++) {
-  //   cluster.fork({ WORKER_TYPE: "app" });
-  // }
-
-  // cluster.on("exit", (worker: ExtendedWorker, code, signal) => {
-  //   logger.warn(
-  //     `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
-  //   );
-  //   debugApp(`Worker ${worker.process.pid} exited`);
-  //   logger.info("Starting a new worker...");
-  //   cluster.fork({ WORKER_TYPE: worker.process.env.WORKER_TYPE });
-  // });
-} else {
-  // Start the HTTP server in this worker
-  const app = express();
-  // Configure middleware
-  configureMiddleware(app);
-  // // Configure routes
-  configureRoutes(app);
-  const server = app.listen(PORT, async () => {
-    try {
-      debugApp(`Worker ${process.pid} attempting to connect to DB and Redis`);
-      await connectDB();
-      logger.info(`Worker ${process.pid} started on port ${PORT}`);
-
-      debugApp(`Worker ${process.pid} successfully started`);
-    } catch (error: any) {
-      logger.error("Failed to start server:", error.message);
-      debugApp(`Worker ${process.pid} failed: ${error.message}`);
-      process.exit(1);
-    }
-  });
-
-  // Graceful shutdown handling
-  process.on("SIGTERM", () => gracefulShutdown(server, "SIGTERM"));
-  process.on("SIGINT", () => gracefulShutdown(server, "SIGINT"));
-
-  if (isDev) {
-    logger.info("Running in development mode with hot-reloading enabled.");
-    debugApp("Debug mode active with hot-reloading");
+    debugApp(`Worker ${process.pid} successfully started`);
+  } catch (error: any) {
+    logger.error("Failed to start server:", error.message);
+    debugApp(`Worker ${process.pid} failed: ${error.message}`);
+    process.exit(1);
   }
+});
+
+// Graceful shutdown handling
+process.on("SIGTERM", () => gracefulShutdown(server, "SIGTERM"));
+process.on("SIGINT", () => gracefulShutdown(server, "SIGINT"));
+
+if (isDev) {
+  logger.info("Running in development mode with hot-reloading enabled.");
+  debugApp("Debug mode active with hot-reloading");
 }
