c90e31ec2b6dab71c6cda94280d05f70c20bee99
cyno-hoinm <emcuahoi1223@gmail.com>
2025-04-11 16:48:41 +0700

    no-drain-pool

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,4 +1,4 @@
-export const maxPoolSize: number = 10; // Maximum number of connections in the pool
-export const minPoolSize: number = 2; // Minimum number of connections to keep open
+export const maxPoolSize: number = 20; // Maximum number of connections in the pool
+export const minPoolSize: number = 1; // Minimum number of connections to keep open
 export const connectTimeoutMS: number = 10000; // Timeout for initial connection (10 seconds)
 export const socketTimeoutMS: number = 45000; // Timeout for socket inactivity (45 seconds)
