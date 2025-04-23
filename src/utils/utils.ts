07dab2968a982810b3671ac43ae529282fc82323
cyno-hoinm <emcuahoi1223@gmail.com>
2025-04-23 14:19:37 +0700

    length-desciprtion

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -88,7 +88,7 @@ export const removeSensitivity = (payload: any): any => {
 
 export function parseChargeMoneyString(description: string) {
   // Regex to match the pattern: Charge money - voucher{voucherId} - userId{userId} - createdBy{createdBy}
-  const regex = /Charge money - voucher(\d+) - userId(\d+) - createdBy(\d+)/;
+  const regex = /voucher(\d+)-userId(\d+)-createdBy(\d+)/;
   const match = description.match(regex);
 
   if (!match) {
