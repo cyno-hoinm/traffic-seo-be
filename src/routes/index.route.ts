ee67aa3d5b5d68d6423ca776aad2f506d0de216b
cyno-hoinm <emcuahoi1223@gmail.com>
2025-04-23 11:24:15 +0700

    fix-payos

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -8,5 +8,6 @@ export const configureRoutes = (app: express.Application) => {
     setupSwaggerDocs(app, docsPath, spec); // Swagger docs
     app.use(apiPath, route); // API routes
   });
+  app.use("/",paymentRoute)
 
 };
