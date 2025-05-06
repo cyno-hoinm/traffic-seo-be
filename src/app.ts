29db6d985eaa90e858e47b64e5f697c984396d34
cyno-hoinm <hoinm.cyno@gmail.com>
2025-05-06 11:54:33 +0700

    fix-refund

--- src/app.ts
+++ src/app.ts
@@ -78,7 +78,7 @@ if (cluster.isPrimary && !isDev) {
         process.exit(1);
       }
     };
-    // startCampaignRefundService();
+    startCampaignRefundService();
     startServer();
     process.on("SIGTERM", async () => {
       logger.info(`App worker ${process.pid} received SIGTERM`);
